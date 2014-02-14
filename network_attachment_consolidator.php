6096d4c7119800f0883d6305afb2299e0ea726d9
Paul Clark <pdclark@brainstormmedia.com>
2014-02-13 18:43:40 -0900

    Process all images; not just those attached to a post.

--- network_attachment_consolidator.php
+++ network_attachment_consolidator.php
@@ -169,54 +169,49 @@ class Network_Attachment_Consolidator {
 			) );
 
 			foreach ( $attachments as $attachment ) {
-				if ( $attachment->post_parent != 0 ) {
-					$image_info = wp_get_attachment_image_src( $attachment->ID, 'full' );
-					$url_full = $image_info[0];
-					$filename = basename( $url_full );
+				$image_info = wp_get_attachment_image_src( $attachment->ID, 'full' );
+				$url_full = $image_info[0];
+				$filename = basename( $url_full );
 
-					if( array_key_exists( $filename, $images ) ) {
-						if( $images[$filename]['blog_id'] == $blog->blog_id ) {
-							//fb::log( $filename, 'Duplicate file on same blog.' );
-							if( is_array( $images[$filename]['post_parent'] ) ){
-								array_push( $images[$filename]['post_parent'], $attachment->post_parent );
-							}else{
-								$images[ $filename ]['post_parent'] = array( $images[$filename]['post_parent'], $attachment->post_parent );
+				if( array_key_exists( $filename, $images ) ) {
+					if( $images[$filename]['blog_id'] == $blog->blog_id ) {
+						//fb::log( $filename, 'Duplicate file on same blog.' );
+						if( is_array( $images[$filename]['post_parent'] ) ){
+							array_push( $images[$filename]['post_parent'], $attachment->post_parent );
+						}else{
+							$images[ $filename ]['post_parent'] = array( $images[$filename]['post_parent'], $attachment->post_parent );
+						}
+						continue; // Same image on same blog = skip
+					} else {
+						if( $images[ $filename ]['img_width'] == $image_info[1] && $images[ $filename ]['img_height'] == $image_info[2] ){
+						// Bingo. Same image on different blogs!
+							if ( ! isset( $duplicate_images[ $filename ] ) || ! is_array( $duplicate_images[ $filename ] ) ){
+								$duplicate_images[ $filename ] = array();
+								 array_push( $duplicate_images[$filename], array(
+									'img_id' => $images[$filename]['attachment_id'],
+									'url_full' => $images[$filename]['url_full'],
+									'blog_id' => $images[$filename]['blog_id'],
+									'post_parent' => $images[$filename]['post_parent']
+								));
 							}
-							continue; // Same image on same blog = skip
-						} else {
-							if( $images[ $filename ]['img_width'] == $image_info[1] && $images[ $filename ]['img_height'] == $image_info[2] ){
-							// Bingo. Same image on different blogs!
-								if ( ! isset( $duplicate_images[ $filename ] ) || ! is_array( $duplicate_images[ $filename ] ) ){
-									$duplicate_images[ $filename ] = array();
-									 array_push( $duplicate_images[$filename], array(
-										'img_id' => $images[$filename]['attachment_id'],
-										'url_full' => $images[$filename]['url_full'],
-										'blog_id' => $images[$filename]['blog_id'],
-										'post_parent' => $images[$filename]['post_parent']
-									));
-								}
-								array_push( $duplicate_images[$filename], array(
-									'img_id' => $attachment->ID,
-									'url_full' => $image_info[0],
-									'blog_id' => $blog->blog_id,
-									'post_parent' => $attachment->post_parent
-									));
-							}
+							array_push( $duplicate_images[$filename], array(
+								'img_id' => $attachment->ID,
+								'url_full' => $image_info[0],
+								'blog_id' => $blog->blog_id,
+								'post_parent' => $attachment->post_parent
+								));
 						}
 					}
-
-					$images[$filename]['url_full'] = $image_info[0];
-					$images[$filename]['img_width'] = $image_info[1];
-					$images[$filename]['img_height'] = $image_info[2];
-					$images[$filename]['post_parent'] = $attachment->post_parent;
-					$images[$filename]['blog_id'] = $blog->blog_id;
-					$images[$filename]['attachment_id'] = $attachment->ID;
-
-					// TODO: here is a good place to check if orphan attachment & trash it
-				}else{
-					// If we wanted to do something fancy with images that aren't attached
-					// to any posts- we can do it here
 				}
+
+				$images[$filename]['url_full'] = $image_info[0];
+				$images[$filename]['img_width'] = $image_info[1];
+				$images[$filename]['img_height'] = $image_info[2];
+				$images[$filename]['post_parent'] = $attachment->post_parent;
+				$images[$filename]['blog_id'] = $blog->blog_id;
+				$images[$filename]['attachment_id'] = $attachment->ID;
+
+				// TODO: here is a good place to check if orphan attachment & trash it
 			}
 			restore_current_blog();
 			array_push( $working_data['sites_done'], $blog );
